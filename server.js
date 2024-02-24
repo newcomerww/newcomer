1edd6612b800c38f20926abb51a33bc0e2961482
newcomerww <nantaelnahom@gmail.com>
2024-02-24 12:02:50 +0300

    gemini

---
+++ server.js
@@ -0,0 +1,31 @@
+
+const TelegramBot = require('node-telegram-bot-api');
+const botToken = '6626615868:AAGVCOIFIUghAb408UL6dzd8D89JgSLCzR0';
+const bot = new TelegramBot(botToken, { polling: true });
+
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+
+const API_KEY ="AIzaSyCg-JZVIZ1pxZg_ZsCOlC2CPx11cA36qwE";
+
+const genAI = new GoogleGenerativeAI(API_KEY);
+bot.on('message', function (msg) {
+  
+  const messag = msg.text;
+  const chatId = msg.chat.id;
+  const message=messag.substring(0,3);
+  if(messag !== '/start'){
+async function run1() {
+  // For text-only input, use the gemini-pro model
+  const model = genAI.getGenerativeModel({ model: "gemini-pro"});
+
+
+  const result = await model.generateContent(messag);
+  const response = await result.response;
+  bot.sendMessage(chatId,response.text());
+  
+  console.log(messag);
+}
+
+run1();
+}
+});
